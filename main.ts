f3565a1c230df6f6b9019d5d4754ade0aecadd57
Sarah <johndoe@example.com>
2020-01-01 18:02:59 +0100

    r

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function init(): void {
 
     let input: HTMLInputElement = document.querySelector("input");
@@ -19,26 +18,4 @@ function update(_event: Event): void {
         img.style.top = "" + (scale *20) + "%";
     }
 }
-=======
-function init(): void {
-
-    let input: HTMLInputElement = document.querySelector("input");
-    console.log(input)
-    input.addEventListener("input", update);
-}
-
-function update(_event: Event): void {
-    let input: HTMLInputElement = document.querySelector("input");
-
-    let imgs: any = document.getElementsByClassName("img-hover-zoom");
-    for (let i: number = 0; i < imgs.length; i++) {
-        let img: any = imgs[i].children[0];
-        img.style.transform = "scale(" + input.value + ")";
-        let scale: number = Number(input.value);
-
-        img.style.left = "" + (scale) + "%";
-        img.style.top = "" + (scale *20) + "%";
-    }
-}
->>>>>>> cf97ce6ef08b51f24832e6457d34801797e663bf
 window.addEventListener("load", init);
